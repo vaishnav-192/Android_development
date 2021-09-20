0dafe27c95ccebe3746dba6d1121bb0eeb9855d3
vaishnav-192 <kamartivaishnav@gmail.com>
2021-09-20 21:07:10 +0530

    day_6 self_exploration and little development

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 // ignore: camel_case_types
 class homepage extends StatelessWidget {
@@ -12,9 +15,46 @@ class homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Catelog app"),
       ),
+      // body: Center(
+      //   child: Container(
+      //     child: Text("Welcome to $name's $days of android dev"),
+      //   ),
+      // ),
       body: Center(
-        child: Container(
-          child: Text("Welcome to $name's $days of android dev"),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+                child: Container(
+                  child: Text(
+                    "Welcome to $name's $days of android development",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  //height: 40,
+                ),
+              ),
+              // SizedBox(
+              //   height: 10,
+              // ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+                child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.loginRoute);
+                      },
+                      child: Text("Go to Login"),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(120, 40),
+                      ),
+                    ),
+              ),
+            ],
+          ),
         ),
       ),
       drawer: Drawer(),
